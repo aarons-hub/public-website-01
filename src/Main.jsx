5de125bf012f80e473bae7fc24d41e8878099605
aarons-hub <aaron.leigh.sanders@gmail.com>
2026-06-12 10:13:34 +1000

    Removal of 404 redirect

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,12 +1,6 @@
 import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-  BrowserRouter,
-} from "react-router-dom";
+import { Route, Routes, useLocation, BrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage.jsx";
 import Projects from "./Projects.jsx";
 import Services from "./Services.jsx";
@@ -127,25 +121,9 @@ function SiteStylesLoader() {
   return null;
 }
 
-function SpaRedirectHandler() {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const redirectPath = params.get("p");
-    if (redirectPath) {
-      navigate(redirectPath, { replace: true });
-    }
-  }, []);
-
-  return null;
-}
-
 function AppLayout() {
   return (
     <>
-      <SpaRedirectHandler />
       <CoverBgHeightSync />
       <div className="cover-bg"></div>
       <div className="app-shell">
